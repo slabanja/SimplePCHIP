316c1afe560ddc1ed52572835e314d6a5a4fc0dd
Gabriel Gerlero <gerlero@users.noreply.github.com>
2021-07-21 15:12:03 -0300

    Require AbstractVector types

--- test/runtests.jl
+++ test/runtests.jl
@@ -40,28 +40,28 @@ function is_approx_le(a, b)
     a < b || a ≈ b
 end
 
-xs = [0.0 1.0]
-ys = [1.0 1.0]
+xs = [0.0, 1.0]
+ys = [1.0, 1.0]
 test_interpolation_is_piecewise_monotone(xs, ys)
 
-xs = [0.0 1.0]
-ys = [1.0 0.0]
+xs = [0.0, 1.0]
+ys = [1.0, 0.0]
 test_interpolation_is_piecewise_monotone(xs, ys)
 
-xs = [0.0 1.0]
-ys = [0.0 1.0]
+xs = [0.0, 1.0]
+ys = [0.0, 1.0]
 test_interpolation_is_piecewise_monotone(xs, ys)
 
-xs = [0.0 1.0 2.0 100.0]
-ys = [0.0 0.0 1.0   1.0]
+xs = [0.0, 1.0, 2.0, 100.0]
+ys = [0.0, 0.0, 1.0,   1.0]
 test_interpolation_is_piecewise_monotone(xs, ys)
 
-xs = [1.0 2.0 5.0 6.0 10.0 22.0]
-ys = [0.0 1.0 0.0 1.0  0.0  1.0]
+xs = [1.0, 2.0, 5.0, 6.0, 10.0, 22.0]
+ys = [0.0, 1.0, 0.0, 1.0,  0.0,  1.0]
 test_interpolation_is_piecewise_monotone(xs, ys)
 
-xs = [   0.0 10.0 10000.0]
-ys = [-100.0  1.2     1.1]
+xs = [   0.0, 10.0, 10000.0]
+ys = [-100.0,  1.2,     1.1]
 test_interpolation_is_piecewise_monotone(xs, ys)
 
 xs = range(0, stop=2π, length=5000)
@@ -69,23 +69,23 @@ ys = sin.(xs)
 test_interpolation_is_piecewise_monotone(collect(xs), ys)
 
 # Example data from Fritsch and Carlson, SIAM J. NUMER. ANAL. 17 (1980) 238-246.
-xs = [ 0.0  2  3  5  6  8  9   11 12 14 15]
-ys = [10.0 10 10 10 10 10 10.5 15 50 60 85]
+xs = [ 0.0,  2,  3,  5,  6,  8,  9,   11, 12, 14, 15]
+ys = [10.0, 10, 10, 10, 10, 10, 10.5, 15, 50, 60, 85]
 test_interpolation_is_piecewise_monotone(xs, ys)
 
 
 # Test the test helper functions
-@test is_monotone([0.0 0.0 0.0])
-@test is_monotone([0.0 1.0 10.0])
-@test is_monotone([10.0 1.0 0.0])
-@test ! is_monotone([0.0 1.0 0.0])
-@test ! is_monotone([1.0 0.0 1.0])
+@test is_monotone([0.0, 0.0, 0.0])
+@test is_monotone([0.0, 1.0, 10.0])
+@test is_monotone([10.0, 1.0, 0.0])
+@test ! is_monotone([0.0, 1.0, 0.0])
+@test ! is_monotone([1.0, 0.0, 1.0])
 
 
 # Test internal functions
 
 # Make sure the correct interval is identified
-p = Interpolator([1.0 2.0 3.0], [0.0 0.0 0.0])
+p = Interpolator([1.0, 2.0, 3.0], [0.0, 0.0, 0.0])
 for search ∈ (PCHIPInterpolation._pchip_index_linear_search, PCHIPInterpolation._pchip_index_bisectional_search)
     @test search(p, 1.0) == 1
     @test search(p, 1.0 + eps(1.0)) == 1
@@ -105,14 +105,14 @@ end
 @test integrate(p, 1, 2.25) == 0
 @test integrate(p, 1.25, 2.25) == 0
 
-p = Interpolator([1.0 2.0 3.0 4.0], [4.0 3.0 2.0 1.0])
+p = Interpolator([1.0, 2.0, 3.0, 4.0], [4.0, 3.0, 2.0, 1.0])
 @test integrate(p, 1, 4) == 3*3/2 + 3
 @test integrate(p, 1, 2) + integrate(p, 2, 4) == integrate(p, 1, 4)
 @test integrate(p, 1, 2.75) + integrate(p, 2.75, 4) == integrate(p, 1, 4)
 @test integrate(p, 3, 1) == -integrate(p, 1, 3)
 
 # Test out of domain
-p = Interpolator([1.0 2.0 3.0 4.0], [4.0 3.0 2.0 1.0])
+p = Interpolator([1.0, 2.0, 3.0, 4.0], [4.0, 3.0, 2.0, 1.0])
 @test p(1) == 4
 @test p(4) == 1
 @test_throws DomainError p(1 - 1e-6)
