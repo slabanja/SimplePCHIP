e92f53c8eea2aefcc71e8f59e1b9322d5c6c89b5
Gabriel Gerlero <gerlero@users.noreply.github.com>
2023-02-01 14:16:58 -0300

    Update search dispatch heuristic

--- src/PCHIPInterpolation.jl
+++ src/PCHIPInterpolation.jl
@@ -104,7 +104,7 @@ end
 function _pchip_index(pchip :: Interpolator, x)
     @argcheck (pchip.xs[1] <= x <= pchip.xs[end]) DomainError
     N = length(pchip.xs)
-    if N < 200  # Approximate performance cross-over on my old intel i7-3517U
+    if N < 25 # Approximate performance cross-over on my M1 MacBook Air
         i = _pchip_index_linear_search(pchip, x)
     else
         i = _pchip_index_bisectional_search(pchip, x)
